dc631b08dc074b9f5fb8642fb22aed00387345cb
luiscasas04 <57513983+luiscasas04@users.noreply.github.com>
2019-12-01 01:57:51 -0600

    Add files via upload
    Added cycle code, minimum edge code and new edge

--- MATH_2305_Final_Project/test.py
+++ MATH_2305_Final_Project/test.py
@@ -1,6 +1,7 @@
 from functions.reading_writing_functions import get_graph
-from functions.graph_operations import incident_edges, cost, min_cost_incident_edge
-    
+from functions.graph_operations import incident_edges, cost, min_cost_incident_edge, add_new_edge
+
+
 G = get_graph('G1.txt')
 
 print('')
@@ -9,6 +10,7 @@ print('')
 print(f'E(G) = {G[1]}')  #print all edges
 
 #initialize tree
+
 T = ( [3, 1, 4], [ (1, 3), (1, 4) ] )
 
 print('')
@@ -23,3 +25,10 @@ for e in incident_edges(G, T):
 minedge = min_cost_incident_edge(G, T)
 print('')
 print(f'The edge with minimum cost is {minedge}, with a cost of {cost(G, minedge)}')
+
+#print edge with minimum cost
+
+T = add_new_edge(G, T)
+print('')
+print(f'the new tree is: T = { T }')
+
