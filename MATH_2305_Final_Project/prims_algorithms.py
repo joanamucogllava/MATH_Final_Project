035ff0260019619eb359742a5d3ddfd1dcb80fb2
Joana Mucogllava <mucogllavajoana@hotmail.com>
2019-12-08 21:18:23 -0800

    Create prims_algorithms.py

---
+++ MATH_2305_Final_Project/prims_algorithms.py
@@ -0,0 +1,11 @@
+Learn more or give us feedback
+# Prims goes here
+from functions.graph_operations import check_tree, add_new_edge;
+    
+def prims(G, v):
+ 
+    T = ([v], [])
+  
+    while check_tree(G,T) == 0:
+        T = add_new_edge(G,T)
+    return T
